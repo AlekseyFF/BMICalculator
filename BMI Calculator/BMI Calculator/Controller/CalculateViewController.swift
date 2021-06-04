f4268a0ee11c49977e301af886fcbb30d18a4f92
Aleksey Fedorov <83182177+AlekseyFF@users.noreply.github.com>
2021-06-04 23:07:38 +0300

    Add files via upload

---
+++ BMI Calculator/BMI Calculator/Controller/CalculateViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Aleksey Fedorov on 01.06.2021.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+    
+    var calculatorBrain = CalculatorBrain()
+
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func heighterSlider(_ sender: UISlider) {
+        let height = (String(format: "%.2f", sender.value))
+        heightLabel.text = "\(height)m"
+    }
+    
+  
+    @IBAction func weighterSlider(_ sender: UISlider) {
+        let weight = (String(format: "%.0f", sender.value))
+        weightLabel.text = "\(weight)kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        calculatorBrain.calculateBMI(height: height, weight: weight)
+        performSegue(withIdentifier: "goToResult", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
+        }
+    }
+   
+}
+
---
+++ BMI Calculator/BMI Calculator/Model/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Aleksey Fedorov on 04.06.2021.
+//
+
+import UIKit
+
+struct BMI {
+    let value : Float
+    let advice: String
+    let color : UIColor
+}
